1dec51cbca66f0eb06f901fed6e3efa3560374f5
VIRSUS67 <101759608+VIRSUS67@users.noreply.github.com>
2022-03-17 00:07:42 +0000

    Add files via upload

---
+++ index.js
@@ -0,0 +1,53 @@
+const express = require("express");
+const app = express();
+
+app.listen(() => console.log("Server started"));
+
+app.use('/ping', (req, res) => {
+  res.send(new Date());
+});
+
+
+const Discord = require('discord.js');
+const cmd = require("node-cmd");
+const ms = require("ms");
+const fs = require('fs');
+const ytdl = require("ytdl-core");
+const canvas = require("canvas");
+const convert = require("hh-mm-ss")
+const fetchVideoInfo = require("youtube-info");
+const simpleytapi = require('simple-youtube-api')
+const util = require("util")
+const gif = require("gif-search");
+const jimp = require("jimp");
+const guild = require('guild');
+const hastebins = require('hastebin-gen');
+const getYoutubeID = require('get-youtube-id');
+const pretty = require("pretty-ms");
+const moment = require('moment');
+const request = require('request');
+const dateFormat = require('dateformat');
+const ffmpeg = require('ffmpeg');
+const ffmpegstatic = require('ffmpeg-static');
+
+const client = new Discord.Client();
+
+////////////
+
+
+client.on('ready',async () => {
+console.log("Starting..");
+let g = client.guilds.get("829441636899618856"); // id server
+let c = g.channels.get("922194969274515497");// id channel
+if(c.type === 'voice') {
+c.join();
+setInterval(() => {
+if(g.me.voiceChannel && g.me.voiceChannelID !== c.id || !g.me.voiceChannel) c.join();
+}, 1);
+} else {
+console.log('Failed To Join: \n The Channel Type isn "Listening."')
+}
+});
+
+
+client.login(process.env.token);
